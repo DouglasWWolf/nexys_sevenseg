818b8d2f34b2665891eebfee343a36a5c0fc6b55
DouglasWWolf <douglaswolf@yahoo.com>
2024-01-23 18:07:43 -0800

    No commit message

--- src/double_dabble.v
+++ src/double_dabble.v
@@ -10,20 +10,23 @@ module double_dabble#(parameter INPUT_WIDTH=1,  parameter DECIMAL_DIGITS=1)
     output                            DONE
 );
 
+// The number of bits in the BCD output
+localparam BCD_BITS = DECIMAL_DIGITS * 4;
+
 // State of the state machine
-reg                        fsm_state;
+reg                   fsm_state;
 
 // Current estimate of the BCD value
-reg [DECIMAL_DIGITS*4-1:0] bcd;
+reg [BCD_BITS-1:0]    bcd;
 
 // Each BCD digit adjusted +3 as neccessary
-wire[DECIMAL_DIGITS*4-1:0] next_bcd;
+wire[BCD_BITS-1:0]    next_bcd;
 
 // Holds the bits of the original input value
-reg [INPUT_WIDTH-1:0]      binary;
+reg [INPUT_WIDTH-1:0] binary;
 
 // We will perform as many shifts as we have input bits
-reg [7:0]                  counter;
+reg [7:0]             counter;
 
 //=============================================================================
 // This block generates "next_bcd" from "bcd".  Each nybble of next_bcd
@@ -47,15 +50,14 @@ always @(posedge clk) begin
     else case (fsm_state)
 
         0:  if (START) begin
-                bcd       <= 0;
-                bcd[0]    <= BINARY[INPUT_WIDTH-1];
+                bcd       <= BINARY[INPUT_WIDTH-1];
                 binary    <= BINARY << 1;
                 counter   <= INPUT_WIDTH-1;
                 fsm_state <= 1;
             end
 
         1:  if (counter) begin
-                bcd     <= {next_bcd[DECIMAL_DIGITS*4-2:0], binary[INPUT_WIDTH-1]};
+                bcd     <= {next_bcd[BCD_BITS-2:0], binary[INPUT_WIDTH-1]};
                 binary  <= binary << 1;
                 counter <= counter - 1;
             end else begin
